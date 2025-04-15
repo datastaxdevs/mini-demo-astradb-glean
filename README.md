0c3444b1f5378d6365a4c7810496f2fa2df45367
Stefano Lottini <stefano.lottini@datastax.com>
2025-04-15 23:37:23 +0200

    script and readme first pass

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # mini-demo-astradb-glean
-Demo showing how to index AstraDB data into Glean
 
-You can follow this tutorial fully in a google collab or follow the instructions below to run locally
+Demo showing how to index [Astra DB](https://docs.datastax.com/en/astra-db-serverless/index.html) data into Glean.
 
-## Work in a Collab
+You can run this tutorial entirely in a google colab, or run it locally by following the instructions below.
+
+## Work in a Colab
 
 [![Open In Colab](https://img.shields.io/badge/Open%20in%20Colab-blue?logo=google-colab&style=for-the-badge)](https://colab.research.google.com/github/datastaxdevs/mini-demo-astradb-glean/blob/main/AstraDB_Glean_Integration.ipynb)
 
@@ -12,9 +13,9 @@ You can follow this tutorial fully in a google collab or follow the instructions
 [![Run Locally](https://img.shields.io/badge/Run%20Locally-python3-blue?style=for-the-badge)](#)
 
 
-### 1.1 Setup AstraDB
+### 1.1 Set up Astra DB
 
-ℹ️ [Astra Reference documentation](https://docs.datastax.com/en/astra-db-serverless/databases/create-database.html)
+ℹ️ See the [Astra Reference documentation](https://docs.datastax.com/en/astra-db-serverless/databases/create-database.html).
 
 
 `✅ 1.1.a`: Create an Astra ACCOUNT
@@ -24,26 +25,25 @@ Access [https://astra.datastax.com](https://astra.datastax.com) and register wit
 ![](https://github.com/datastaxdevs/mini-demo-astradb-glean/blob/main/images/01-login.png?raw=true)
 
 
-`✅ 1.1.b`: Create an Astra Database
+`✅ 1.1.b`: Create a Database in Astra DB
 
 Get to the databases dashboard (by clicking on Databases in the left-hand navigation bar, expanding it if necessary), and click the `[Create Database]` button on the right.
 
 ![](https://github.com/datastaxdevs/mini-demo-astradb-glean/blob/main/images/02-create-db.png?raw=true)
 
 
-- **ℹ️ Fields Description**
+- **ℹ️ Field Description**
 
 | Field                                      | Description                                                                                                                                                                                                                                   |
 |--------------------------------------------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
-| **Vector Database vs Serverless Database** | Choose `Vector Database` In june 2023, Cassandra introduced the support of vector search to enable Generative AI use cases.                                                                                                                   |
-| **Database name**                          | It does not need to be unique, is not used to initialize a connection, and is only a label (keep it between 2 and 50 characters). It is recommended to have a database for each of your applications. The free tier is limited to 5 databases. |
-| **Cloud Provider**                         | Choose whatever you like. Click a cloud provider logo, pick an Area in the list and finally pick a region. We recommend choosing a region that is closest to you to reduce latency. In free tier, there is very little difference.            |
-| **Cloud Region**                           | Pick region close to you available for selected cloud provider and your plan.      
+| **Vector Database vs Serverless Database** | Choose `Vector Database`. In june 2023, Cassandra introduced the support of vector search to enable Generative AI use cases.                                                                                                                   |
+| **Database name**                          | Database names are permanent. They must start and end with a letter or number, and they can contain no more than 50 characters, including letters, numbers, and the special characters `& + - _ ( ) < > . , @`. It is recommended to have a database for each of your applications. The free tier is limited to 5 databases. |
+| **Cloud Provider**                         | Choose whatever you like. Click a cloud provider logo, pick an Area in the list and finally pick a region. We recommend choosing a region that is closest to you to reduce latency. In the free tier, there is very little difference.            |
+| **Cloud Region**                           | Pick a region close to you, among those available for the selected cloud provider and your plan.      
 
 If all fields are filled properly, clicking the "Create Database" button will start the process.
 
 ![](https://github.com/datastaxdevs/mini-demo-astradb-glean/blob/main/images/03-pending-db.png?raw=true)
-
 
 It should take a couple of minutes for your database to become `Active`.
 
@@ -51,49 +51,50 @@ It should take a couple of minutes for your database to become `Active`.
 
 `✅ 1.1.c`: Create an Astra TOKEN
 
-To connect to your database, you need the API Endpoint and a token. The api endpoint is available on the database screen, there is a little icon to copy the URL in your clipboard. (it should look like `https://<db-id>-<db-region>.apps.astra.datastax.com`).
+To [connect to your database](https://docs.datastax.com/en/astra-db-serverless/get-started/quickstart.html#create-a-database-and-store-your-credentials), you need the **API endpoint** and a **Database token**.
+
+The API endpoint is available on the database screen, there is a little icon to copy the URL in your clipboard. (it should look like `https://<db-id>-<db-region>.apps.astra.datastax.com`).
 
 ![](https://github.com/datastaxdevs/mini-demo-astradb-glean/blob/main/images/05-create-token-db.png?raw=true)
 
 To get a token click the `[Generate Token]` button on the right. It will generate a token that you can copy to your clipboard.
 
-
 ## 2. Installation
 
 ### 2.1 Python Environment
 
-- `✅ 2.1.a`: Create and activate a virtual environment
+- `✅ 2.1.a`: Create and activate a virtual environment. You need Python version 3.9 or higher.
 
 ```console
-python3 -m venv venv
+python3 -m venv my_virtual_env
 ```
 
-_macOS_
+_macOS/Linux:_
 ```
-source venv/bin/activate
-```
-
-_Windows_
-```
-venv\Scripts\activate
+source my_virtual_env/bin/activate
 ```
 
-- `✅ 2.1.b`:Install the dependencies
+_Windows:_
+```
+my_virtual_env\Scripts\activate
+```
+
+- `✅ 2.1.b`:Install the dependencies:
 
 ```console
-pip install astrapy==1.4.1 --no-deps
 pip install -r requirements.txt
 ```
 
-- `✅ 2.1.c`: Edit `.env`
+- `✅ 2.1.c`: Create an environment file (`.env`):
 
-_Copy `.env.example` as `.env`_
+Copy `.env.example` as `.env`, and edit its content with the Astra DB and Glean credentials:
 
 ```ini
 # Astra Configuration
 export ASTRA_DB_APPLICATION_TOKEN=<change_me>
 export ASTRA_DB_API_ENDPOINT=<change_me>
 export ASTRA_DB_COLLECTION_NAME="plain_collection"
+# export ASTRA_DB_KEYSPACE="default_keyspace"  # Optional
 
 # Glean Configuration
 export GLEAN_CUSTOMER=<you>
@@ -101,13 +102,12 @@ export GLEAN_DATASOURCE_NAME=<change_me>
 export GLEAN_API_TOKEN=<change_me>
 ```
 
-
-- `✅ 2.1.d`:Run the script
+## 3. Run the script
 
 ```console
 python3 astra-glean-import-job.py
 ```
 
+## 4. More information
 
-
-
+ℹ️ [Glean integration page](https://docs.datastax.com/en/astra-db-serverless/integrations/glean.html) on Astra DB documentation.
--- astra-glean-import-job.py
+++ astra-glean-import-job.py
@@ -1,81 +1,107 @@
 import os
-from dotenv import load_dotenv
-from getpass import getpass
-import pandas as pd
+
 from astrapy import DataAPIClient
-from tqdm import tqdm  # Progress bar with tqdm
-import json
+from colorama import Fore, Style
+from datasets import load_dataset
+from dotenv import load_dotenv
+
 import glean_indexing_api_client as indexing_api
 from glean_indexing_api_client.api import datasources_api, documents_api
-from glean_indexing_api_client.model.custom_datasource_config import CustomDatasourceConfig
+from glean_indexing_api_client.model.custom_datasource_config import (
+    CustomDatasourceConfig,
+)
 from glean_indexing_api_client.model.object_definition import ObjectDefinition
 from glean_indexing_api_client.model.index_document_request import IndexDocumentRequest
 from glean_indexing_api_client.model.document_definition import DocumentDefinition
 from glean_indexing_api_client.model.content_definition import ContentDefinition
-from glean_indexing_api_client.model.document_permissions_definition import DocumentPermissionsDefinition
-from datasets import load_dataset
-from colorama import Fore, Style  # For color coding the output
+from glean_indexing_api_client.model.document_permissions_definition import (
+    DocumentPermissionsDefinition,
+)
+
 
 # Load environment variables from .env
 load_dotenv()
 
-ASTRA_DB_APPLICATION_TOKEN = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
-ASTRA_DB_API_ENDPOINT      = os.getenv("ASTRA_DB_API_ENDPOINT")
-ASTRA_DB_COLLECTION_NAME   = os.getenv("ASTRA_DB_COLLECTION_NAME")
-GLEAN_API_TOKEN            = os.getenv("GLEAN_API_TOKEN")
-GLEAN_CUSTOMER             = os.getenv("GLEAN_CUSTOMER")
-GLEAN_DATASOURCE_NAME      = os.getenv("GLEAN_DATASOURCE_NAME")
+ASTRA_DB_APPLICATION_TOKEN = os.environ["ASTRA_DB_APPLICATION_TOKEN"]
+ASTRA_DB_API_ENDPOINT = os.environ["ASTRA_DB_API_ENDPOINT"]
+ASTRA_DB_COLLECTION_NAME = os.environ["ASTRA_DB_COLLECTION_NAME"]
+ASTRA_DB_KEYSPACE = os.getenv("ASTRA_DB_KEYSPACE")
+
+GLEAN_API_TOKEN = os.environ["GLEAN_API_TOKEN"]
+GLEAN_CUSTOMER = os.environ["GLEAN_CUSTOMER"]
+GLEAN_DATASOURCE_NAME = os.environ["GLEAN_DATASOURCE_NAME"]
+
 
 print(f"{Fore.GREEN}============================={Style.RESET_ALL}")
 print(f"{Fore.GREEN} ASTRADB - GLEAN INTEGRATION {Style.RESET_ALL}")
 print(f"{Fore.GREEN}============================={Style.RESET_ALL}\n")
 
 # Initialize Astra DB client
-client = DataAPIClient(ASTRA_DB_APPLICATION_TOKEN, caller_name="glean", caller_version="1.0")
-database = client.get_database(ASTRA_DB_API_ENDPOINT)
-print(f"{Fore.CYAN}[ OK ] - Credentials are OK, your database name is {Style.RESET_ALL}{database.info().name}")
+client = DataAPIClient(callers=[("glean", "1.0")])
+database = client.get_database(
+    ASTRA_DB_API_ENDPOINT,
+    token=ASTRA_DB_APPLICATION_TOKEN,
+    keyspace=ASTRA_DB_KEYSPACE,
+)
+print(
+    f"{Fore.CYAN}[ OK ] - Credentials are OK, your database name is "
+    f"{Style.RESET_ALL}{database.name()}{Fore.CYAN}."
+)
 
 # Create collection
-plain_collection = database.create_collection(ASTRA_DB_COLLECTION_NAME, check_exists=False)
-print(f"{Fore.CYAN}[ OK ] - Collection {Style.RESET_ALL}{ASTRA_DB_COLLECTION_NAME}{Fore.CYAN} is ready{Style.RESET_ALL}")
+source_collection = database.create_collection(ASTRA_DB_COLLECTION_NAME)
+print(
+    f"{Fore.CYAN}[ OK ] - Collection {Style.RESET_ALL}{source_collection.name}"
+    f"{Fore.CYAN} is ready{Style.RESET_ALL}{Fore.CYAN}."
+)
 
-# Load philosopher dataset
-print(f"{Fore.CYAN}[INFO] - Downloading Data from Hugging Face 🤗{Style.RESET_ALL}")
+# Load philosophers dataset
+print(f"{Fore.CYAN}[INFO] - Downloading data from Hugging Face 🤗.{Style.RESET_ALL}")
 philo_dataset = load_dataset("datastax/philosopher-quotes")["train"]
-print(f"{Fore.CYAN}[ OK ] - Dataset loaded in memory{Style.RESET_ALL}")
-print(f"{Fore.CYAN}[INFO] - Record: {Style.RESET_ALL}{philo_dataset[16]}")
-philo_dataframe = pd.DataFrame.from_dict(philo_dataset)
+print(f"{Fore.CYAN}[ OK ] - Dataset loaded in memory.{Style.RESET_ALL}")
+print(f"{Fore.CYAN}[INFO] - Sample record: {Style.RESET_ALL}{philo_dataset[16]}")
+
 
 # Progress bar for loading to Astra
-def load_to_astra(df, collection):
-    len_df = len(df)
-    print(f"{Fore.CYAN}[INFO] - Starting data insertion into Astra DB...{Style.RESET_ALL}")
-    
-    for i in tqdm(range(len_df), desc="Inserting documents", colour="green"):
-        try:
-            collection.insert_one({
-                "_id": i,
-                "author": df.loc[i, "author"],
-                "quote": df.loc[i, "quote"],
-                "tags": df.loc[i, "tags"].split(";") if pd.notna(df.loc[i, "tags"]) else []
-            })
-        except Exception as error:
-            print(f"{Fore.RED}Error while inserting document {i}: {error}{Style.RESET_ALL}")
+def load_to_astra_db(data_to_insert, collection):
+    def split_tags(t):
+        return [tag for tag in (t or "").split(";") if tag]
 
-# Flush the collection before inserting new data
-plain_collection.delete_many({})
-print(f"{Fore.CYAN}[ OK ] - Collection has been flushed{Style.RESET_ALL}")
+    documents_to_insert = [
+        {
+            **item,
+            **{"_id": index, "tags": split_tags(item["tags"])},
+        }
+        for index, item in enumerate(data_to_insert)
+    ]
+    collection.insert_many(documents_to_insert)
+
+
+# Empty the collection before inserting fresh data
+# (WARNING: it may wipe out actual data. We are doing it for DEMO PURPOSES here.)
+source_collection.delete_many({})
+print(f"{Fore.CYAN}[ OK ] - Collection has been emptied.{Style.RESET_ALL}")
 
 # Insert documents into Astra DB
-load_to_astra(philo_dataframe, plain_collection)
-print(f"{Fore.CYAN}[ OK ] - Load finished{Style.RESET_ALL}")
+philo_count = len(philo_dataset)
+print(
+    f"{Fore.CYAN}[INFO] - Inserting {philo_count} documents into Astra DB..."
+    f"{Style.RESET_ALL}"
+)
+load_to_astra_db(philo_dataset, source_collection)
+print(f"{Fore.CYAN}[ OK ] - Insertion finished.{Style.RESET_ALL}")
 
 # Setup Glean API
 GLEAN_API_ENDPOINT = f"https://{GLEAN_CUSTOMER}-be.glean.com/api/index/v1"
-print(f"{Fore.CYAN}[INFO] - Glean API setup, endpoint is:{Style.RESET_ALL} {GLEAN_API_ENDPOINT}")
+print(
+    f"{Fore.CYAN}[INFO] - Glean API setup, endpoint is:"
+    f"{Style.RESET_ALL} {GLEAN_API_ENDPOINT}"
+)
 
 # Initialize Glean client
-configuration = indexing_api.Configuration(host=GLEAN_API_ENDPOINT, access_token=GLEAN_API_TOKEN)
+configuration = indexing_api.Configuration(
+    host=GLEAN_API_ENDPOINT, access_token=GLEAN_API_TOKEN
+)
 api_client = indexing_api.ApiClient(configuration)
 datasource_api = datasources_api.DatasourcesApi(api_client)
 print(f"{Fore.CYAN}[ OK ] - Glean client initialized{Style.RESET_ALL}")
@@ -83,35 +109,39 @@ print(f"{Fore.CYAN}[ OK ] - Glean client initialized{Style.RESET_ALL}")
 # Create and register datasource in Glean
 datasource_config = CustomDatasourceConfig(
     name=GLEAN_DATASOURCE_NAME,
-    display_name='AstraDB Collection DataSource',
-    datasource_category='PUBLISHED_CONTENT',
-    url_regex='^https://your_astra_db_url',  # Replace with actual regex
+    display_name="AstraDB Collection DataSource",
+    datasource_category="PUBLISHED_CONTENT",
+    url_regex=f"^{ASTRA_DB_API_ENDPOINT}",
     object_definitions=[
-        ObjectDefinition(
-            doc_category='PUBLISHED_CONTENT',
-            name='AstraVectorEntry'
-        )
-    ]
+        ObjectDefinition(doc_category="PUBLISHED_CONTENT", name="AstraVectorEntry")
+    ],
 )
 
 try:
     datasource_api.adddatasource_post(datasource_config)
-    print(f"{Fore.GREEN}[ OK ] - DataSource has been created!{Style.RESET_ALL}")
+    print(
+        f"{Fore.GREEN}[ OK ] - DataSource has been created!"
+        f"{Style.RESET_ALL}{Fore.GREEN}."
+    )
 except indexing_api.ApiException as e:
-    print(f"{Fore.RED}[ ERROR ] - Error creating datasource: {e}{Style.RESET_ALL}")
+    print(
+        f"{Fore.RED}[ ERROR ] - Error creating datasource:"
+        f"{e}{Style.RESET_ALL}{Fore.GREEN}."
+    )
 
-# Function to index Astra documents into Glean
-def index_astra_document_into_glean(astra_document):
-    document_id = str(astra_document['_id'])
-    title = astra_document['author'] + ' quote_' + str(astra_document['_id'])
-    body_text = astra_document['quote']
+
+def index_astra_db_document_into_glean(astra_document):
+    """Index one Astra DB document into Glean."""
+    document_id = str(astra_document["_id"])
+    title = f"{astra_document['author']} quote_{astra_document['_id']}"
+    body_text = astra_document["quote"]
     datasource_name = GLEAN_DATASOURCE_NAME
     request = IndexDocumentRequest(
         document=DocumentDefinition(
             datasource=datasource_name,
             title=title,
             id=document_id,
-            view_url="https://your_astra_db_url",
+            view_url=ASTRA_DB_API_ENDPOINT,
             body=ContentDefinition(mime_type="text/plain", text_content=body_text),
             permissions=DocumentPermissionsDefinition(allow_anonymous_access=True),
         )
@@ -122,20 +152,26 @@ def index_astra_document_into_glean(astra_document):
     except indexing_api.ApiException as e:
         print(f"{Fore.RED}Error indexing document {document_id}: {e}{Style.RESET_ALL}")
 
-# Index documents from Astra DB to Glean
+
 def index_documents_to_glean(collection):
-    total_docs = collection.estimated_document_count() 
-    with tqdm(total=total_docs, desc="Indexing documents to Glean", unit="doc", colour="blue") as pbar:
-        for doc in collection.find():
-            try:
-                index_astra_document_into_glean(doc)
-                pbar.set_postfix({"Status": f"Indexed {doc['_id']}"})
-            except Exception as error:
-                pbar.set_postfix({"Status": f"Error with {doc['_id']}"})
-                print(f"{Fore.RED}Error indexing document {doc['_id']}: {error}{Style.RESET_ALL}")
-            pbar.update(1)  # Update progress bar after each document
+    """Index all documents from an Astra DB collection to Glean."""
+    total_docs = collection.count_documents({}, upper_bound=1000)
+    print(
+        f"{Fore.CYAN}[INFO] - Indexing {total_docs} "
+        f"documents into Glean...{Style.RESET_ALL}"
+    )
+    for doc in collection.find():
+        try:
+            index_astra_db_document_into_glean(doc)
+        except Exception as error:
+            print(
+                f"{Fore.RED}Error indexing document "
+                f"{doc['_id']}: {error}{Style.RESET_ALL}"
+            )
+    print(f"{Fore.CYAN}[ OK ] - Indexing finished.{Style.RESET_ALL}")
+
 
 # Use the function to index documents into Glean
-index_documents_to_glean(plain_collection)
+index_documents_to_glean(source_collection)
 
-print(f"{Fore.GREEN}Batch Ended Successfully!{Style.RESET_ALL}")
+print(f"{Fore.GREEN}Import job completed successfully!{Style.RESET_ALL}")
